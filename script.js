363312afebb291cb5aa905798626a22f60f1e085
Nathan66788 <nathanbosma@outlook.com>
2025-04-02 15:34:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.getElementById('play-button').addEventListener('click', () => {
+    window.location.href = 'levels.html';
+});
 
+document.getElementById('settings-button').addEventListener('click', () => {
+    alert('Settings menu opened!');
+    // ...future code...
+});
+
+document.getElementById('sound-button').addEventListener('click', () => {
+    alert('Sound toggled!');
+    // ...future code...
+});

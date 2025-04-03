8b05482c8e0b81e2b1970fca212d95fca64059d5
MiraeNassr <miraenassr@gmail.com>
2025-04-03 11:37:58 +0200

    update to game place html file

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ document.getElementById('play-button').addEventListener('click', () => {
     window.location.href = 'levels.html';
 });
 
+document.getElementById('start-game-button').addEventListener('click', () => {
+    window.location.href = 'gamePlace.html';
+});
+
 document.getElementById('settings-button').addEventListener('click', () => {
     alert('Settings menu opened!');
     // ...future code...
